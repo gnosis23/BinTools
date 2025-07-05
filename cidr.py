c1a748349027b875da50041e2b4f10a74be88c76
BOHAO WANG <bj050323@gmail.com>
2025-07-05 22:47:37 +0800

    misc: format

--- cidr.py
+++ cidr.py
@@ -2,10 +2,10 @@
 from netaddr import *
 import sys
 
-#print(sys.argv)
+# print(sys.argv)
 if len(sys.argv) < 3:
     print("## need 2 ip address")
     exit()
 
-cidrs = iprange_to_cidrs(IPAddress(sys.argv[1]) , IPAddress(sys.argv[2]))
+cidrs = iprange_to_cidrs(IPAddress(sys.argv[1]), IPAddress(sys.argv[2]))
 print(cidrs)
